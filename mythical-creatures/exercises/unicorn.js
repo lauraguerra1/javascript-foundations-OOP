5bcdf49e50a16ce01a3c17674195733c2e265fb0
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-20 17:59:05 -0500

    Pass unicorn tests and create additional test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,17 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
+    this.says = function(speech) {
+      return `**;* ${speech} *;**`
+    }
+  }
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
