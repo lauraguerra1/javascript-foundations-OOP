c047ac0677b2afefd9e3d56f6ea588a8f36b899a
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-20 18:47:20 -0500

    Pass dragon tests and create additional tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.timesAte = 0
+        this.greet = function() {
+            return `Hi, ${rider}!`
+        }
+    }
+    eat() {
+        this.timesAte += 1
+        if (this.timesAte === 3) {
+            this.hungry = false;
+            this.timesAte = 0
+        }
+    }
+    fly() {
+        this.hungry = true
+    }
+}
 
+module.exports = Dragon;
