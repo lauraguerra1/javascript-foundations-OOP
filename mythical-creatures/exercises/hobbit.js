a982eca521084184622d3b3e6c645edad34de48d
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-20 20:03:02 -0500

    Pass hobbit tests

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,27 @@
+class Hobbit {
+    constructor(hobbit) {
+        this.name = hobbit.name;
+        this.age = 0;
+        this.adult = false;
+        this.old = false;
+    }
+    celebrateBirthday() {
+        this.age += 1;
+        if (this.age > 100) {
+            this.old = true;
+        } else if (this.age > 32) {
+            this.adult = true;
+        } 
+    }
+    getRing() {
+        if (this.name === 'Frodo') {
+            this.hasRing = true;
+            return `Here is the ring!`;
+        } else {
+            this.hasRing = false;
+            return `You can't have it!`;
+        }
+    }
+}
+
+module.exports = Hobbit;
