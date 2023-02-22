55c9a4d28ff19c7408779e6411c135a32fd11c40
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-22 11:33:49 -0500

    Pass fairy tests

---
+++ mythical-creatures/exercises/fairy.js
@@ -0,0 +1,36 @@
+class Fairy {
+    constructor(name) {
+        this.name = name;
+        this.dust = 10;
+        this.clothes = {dresses: ['Iris']};
+        this.disposition = 'Good natured';
+        this.humanWards = [];
+    }
+    receiveBelief() {
+        this.dust += 1;
+    }
+    believe() {
+        this.dust += 10;
+    }
+    makeDresses(flowers) {
+        for (var i = 0; i < flowers.length; i++) {
+            this.clothes.dresses.push(flowers[i]);
+        }
+    }
+    becomeProvoked() {
+        this.disposition = 'Vengeful';
+    }
+    replaceInfant(infant) {
+        if (this.humanWards.length === 2) {
+            this.humanWards.push(infant);
+            this.disposition = 'Good natured';
+        } else if (this.humanWards.length < 3 && this.disposition === 'Vengeful') {
+            infant.disposition = 'Malicious'; 
+            this.humanWards.push(infant);
+        } else {
+            return infant;
+        }
+    }
+}
+
+module.exports = Fairy;
