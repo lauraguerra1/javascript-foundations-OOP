30396c31f86420847d50ee970d95b5d30ee91edc
Laura Guerra <l.garciaguerra1@gmail.com>
2023-03-07 15:36:31 -0500

    Pass lunchbox tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,24 @@
 class LunchBox {
-
+    constructor (lunchbox) {
+        this.owner = lunchbox.owner; 
+        this.material = lunchbox.madeOf;
+        this.shape = lunchbox.shape;
+        this.color = lunchbox.color;
+        this.snacks = [];
+    }
+    acquireSnack(snack) {
+        this.snacks.push(snack);
+        snack.isInLunchBox = true;
+    }
+    findHealthySnacks() {
+        var healthySnacks = []
+        this.snacks.forEach((snack) => {
+            if (snack.checkForHealthy()) {
+                healthySnacks.push(snack.type);
+            };
+        })
+        return healthySnacks;
+    }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,25 @@
 class Snack {
-
+    constructor(type) {
+        this.type = type; 
+        this.deliciousLevel = 'extra';
+        this.amount = 100;
+        this.isInLunchBox = false;
+     }
+     getEaten(){
+        this.amount -= 10;
+        if(this.amount <= 20) {
+            this.cuttingItClose = true
+        } else {
+            this.cuttingItClose = false;
+        }
+     }
+     checkForHealthy() {
+        if (this.type.toLowerCase().includes('fruit')) {
+            return true;
+        } else {
+            return false;
+        }
+     }
 }
 
 module.exports = Snack;
