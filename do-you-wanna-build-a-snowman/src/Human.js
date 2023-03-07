2c573bdcafb0dc79724b85eee08b268db2349c97
Laura Guerra <l.garciaguerra1@gmail.com>
2023-03-07 09:30:04 -0500

    Pass snowman tests

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,34 @@
-var Snowman = require('./Snowman');
+var Snowman = require("./Snowman");
 
 class Human {
-
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0,
+    };
+  }
+  gatherMaterials(material, quantity) {
+    Object.keys(this.materials).forEach((key) => {
+      if (key === material) {
+        this.materials[key] += quantity;
+      }
+    });
+  }
+  buildASnowman() {
+    return new Snowman(this.materials);
+  }
+  placeMagicHat(snowman) {
+    snowman.canWearMagicHat();
+    if (snowman.magicHat === true) {
+      return `Woah, this snowman is coming to life!`;
+    } else {
+      return `I guess I didn't build it correctly.`;
+    }
+  }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,19 @@
 class Snowman {
-
+  constructor(snowman) {
+    this.carrots = snowman.carrots;
+    this.coal = snowman.coal;
+    this.buttons = snowman.buttons;
+    this.snowballs = snowman.snowballs;
+    this.magicHat = false;
+  }
+  canWearMagicHat() {
+    if (this.coal > 1 && this.buttons > 4 && this.carrots && this.snowballs > 1) {
+      this.magicHat = true;
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Snowman;
